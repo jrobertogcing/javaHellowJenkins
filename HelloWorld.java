cbc5bb34f79802a563a454ffe83341360414154e
J. Roberto <jose.gonzalez@gonet.us>
2019-01-17 09:14:00 -0600

    Test 7

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World. Jenkins push prueba 5");
+        System.out.println("Hello, World. Jenkins push prueba 7");
     }
 
 }
